7abaa933e148b0ffc4e6ed739d15c9e707ecc1df
Vegesna, Vijay Varma <VVegesna@us.imshealth.com>
2021-05-01 04:46:56 -0700

    Initial Commit

---
+++ TestNavigation/TestNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestNavigationApp.swift
+//  TestNavigation
+//
+//  Created by vijay varma vegesna on 5/1/21.
+//
+
+import SwiftUI
+
+@main
+struct TestNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
